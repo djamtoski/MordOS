9193887b1bb26072e8cf1ab6b7b8238f44b41e5e
Marko Djamtoski <mdjamtoski@melontech.com>
2022-05-07 21:19:25 +0200

    Added fake backend and components

--- Mdj-Mord-OS/src/app/app-routing.module.ts
+++ Mdj-Mord-OS/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { fakeBackendProvider } from 'src/helpers';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers : [
+    fakeBackendProvider
+  ]
 })
 export class AppRoutingModule { }
